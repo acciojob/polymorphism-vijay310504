92d3f238f104b8bb47858d19deec59ddab8eecba
VIJAY <nvvijay10@gmail.com>
2026-04-27 22:31:25 +0530

    new

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
-
+import java.util.*;
 public class Main {
+     static class Product{
+        public int product(int x,int y){
+            return x*y;
+        }public int product(int x,int y,int z){
+            return x*y*z;
+        }public double product(double x,double y){
+            return x*y;
+        }
+    }public static void main(String[] args) {
+        Product p=new Product();
+        int res1=p.product(10,20);
+        System.out.println(res1);
+        int res2=p.product(10,20,30);
+        System.out.println(res2);
+        double res3=p.product(10.5,20.5);
+        System.out.println(res3);
+    }
 
 }
